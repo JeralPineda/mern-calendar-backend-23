8f08cd254cbbd3f57b832a84b410e4e4a2b3d95e
Jeral Pineda <jpineda@banhcafe.hn>
2023-05-12 09:12:54 -0600

    Configuracion rutas para servir la app al frontend

--- index.js
+++ index.js
@@ -10,28 +10,24 @@ const app = express();
 dbConnection();
 
 // CORS
-app.use(cors())
+app.use(cors());
 
 // Directorio Público
-app.use( express.static('public') );
+app.use(express.static('public'));
 
 // Lectura y parseo del body
-app.use( express.json() );
+app.use(express.json());
 
 // Rutas
-app.use('/api/auth', require('./routes/auth') );
-app.use('/api/events', require('./routes/events') );
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
 
-
-
-
-// Escuchar peticiones
-app.listen( process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+// Configuración para servir la app (rutas) en frontend en producción
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-
-
-
-
-
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
